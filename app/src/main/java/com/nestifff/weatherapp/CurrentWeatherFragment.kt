b455bbbfb7791ab183285a12a2795aa54679bd65
nestifff <nestiv.01.12@gmail.com>
2021-11-24 18:17:22 +0300

    add theme, bottom_menu and header

--- app/src/main/java/com/nestifff/weatherapp/CurrentWeatherFragment.kt
+++ app/src/main/java/com/nestifff/weatherapp/CurrentWeatherFragment.kt
@@ -1,59 +1,29 @@
 package com.nestifff.weatherapp
 
-import android.content.Context
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.nestifff.weatherapp.models.weather.CurrentWeatherInfo
 
-class CurrentWeatherFragment(
-//    val showForecast: ShowForecast
-) : Fragment() {
+class CurrentWeatherFragment() : Fragment() {
 
     private lateinit var fragmentView: View
-    private var listener: ShowForecast? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         fragmentView = inflater.inflate(R.layout.current_weather_fragment, container, false)
-        fragmentView.findViewById<Button>(R.id.btn_show_forecast).setOnClickListener {
-            listener?.showForecast()
-        }
         return fragmentView
-    }
-
-    fun showLocation(location: Location) {
-        fragmentView.findViewById<TextView>(R.id.tv_location).text = location.toString() +
-                "\n\nlatitude: ${location.latitude}, \nlongitude: ${location.longitude}"
     }
 
     fun showCurrentWeather(currWeather: CurrentWeatherInfo?) {
         fragmentView.findViewById<TextView>(R.id.tv_curr_weather).text =
-            currWeather?.toString() ?: "null"
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is ShowForecast) {
-            listener = context
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    interface ShowForecast {
-        fun showForecast()
+                currWeather?.toString() ?: "null"
     }
 
 }
--- app/src/main/java/com/nestifff/weatherapp/MainActivity.kt
+++ app/src/main/java/com/nestifff/weatherapp/MainActivity.kt
@@ -4,14 +4,21 @@ import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest.PRIORITY_LOW_POWER
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nestifff.weatherapp.forecastFragment.ForecastFragment
 import com.nestifff.weatherapp.models.forecast.ForecastJsonObj
 import com.nestifff.weatherapp.models.weather.CurrentWeatherInfo
@@ -19,121 +26,205 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity(), CurrentWeatherFragment.ShowForecast {
+
+class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var currWeatherFragment: CurrentWeatherFragment
     private lateinit var forecastFragment: ForecastFragment
 
+    private lateinit var tvViewMode: TextView
+
     private lateinit var location: Location
+    private var currentWeather: CurrentWeatherInfo? = null
+    private var cityName: String? = null
 
     private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
+            registerForActivityResult(
+                    ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
 
-                invokeGetLastLocation()
+                    getLastLocationShowCurrWeather()
 
-            } else {
-                Toast.makeText(this, "You are idiot", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "You are idiot", Toast.LENGTH_SHORT).show()
+                }
             }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setThemeAndStatusBarColor()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.supportActionBar?.hide()
+
+        tvViewMode = findViewById(R.id.tv_view_mode)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        forecastFragment = ForecastFragment()
+        currWeatherFragment = CurrentWeatherFragment()
+
+        // TODO restore activity state (after rotate)
+        if (savedInstanceState != null) {
+
+        }
+
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, currWeatherFragment)
+                .commit()
+        tvViewMode.text = getString(R.string.today)
+
+        requestPermissionAndGetLocation()
+
+        findViewById<BottomNavigationView>(R.id.menu_bottom).setOnNavigationItemSelectedListener {
+            val currFragments = supportFragmentManager.fragments
+            when (it.itemId) {
+                R.id.menu_curr_weather -> {
+                    if (currFragments.isNotEmpty() &&
+                            currFragments[currFragments.size - 1] !is CurrentWeatherFragment
+                    ) {
+                        currWeatherFragment = CurrentWeatherFragment()
+                        setCurrentFragment(currWeatherFragment)
+                    }
+                }
+                R.id.menu_forecast -> {
+                    if (currFragments.isNotEmpty() &&
+                            currFragments[currFragments.size - 1] !is ForecastFragment
+                    ) {
+                        forecastFragment = ForecastFragment()
+                        setCurrentFragment(forecastFragment)
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    private fun setThemeAndStatusBarColor() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.light_gray)
+        }
+    }
+
+    private fun requestPermissionAndGetLocation() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            getLastLocationShowCurrWeather()
+            return
+        }
 
         when {
             ContextCompat.checkSelfPermission(
-                this,
-                ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
+                    this,
+                    ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> getLastLocationShowCurrWeather()
 
-                invokeGetLastLocation()
+            shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) -> {
+
+                Toast.makeText(this,
+                        "You should provide your location info to show weather and forecast",
+                        Toast.LENGTH_LONG).show()
+                requestPermissionLauncher.launch(ACCESS_COARSE_LOCATION)
             }
-            /*Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                    shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) -> {
-                Toast.makeText(this, "Please", Toast.LENGTH_SHORT).show()
-            }*/
             else -> {
                 requestPermissionLauncher.launch(ACCESS_COARSE_LOCATION)
             }
         }
+    }
 
-        currWeatherFragment = CurrentWeatherFragment()
+    private fun setCurrentFragment(fragment: Fragment) {
+
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, currWeatherFragment)
-            .commit()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
 
+        if (fragment is ForecastFragment) {
+            tvViewMode.text = cityName ?: getString(R.string.forecast)
+            showForecast()
+        } else if (fragment is CurrentWeatherFragment) {
+            tvViewMode.text = getString(R.string.today)
+            showCurrentWeather()
+        }
     }
 
     @SuppressLint("MissingPermission")
-    private fun invokeGetLastLocation() {
+    private fun getLastLocationShowCurrWeather() {
         fusedLocationClient.lastLocation
-            .addOnSuccessListener { it: Location? ->
-                // Got last known location. In some rare situations this can be null.
-                it?.let {
-
-                    location = it
-
-                    Log.i(TAG, "invokeGetLastLocation: $location")
-                    currWeatherFragment.showLocation(location)
-
-                    ServiceGenerator.getRequestApi()
-                        .getCurrentWeather(
-                            location.latitude,
-                            location.longitude
-                        )
-                        .enqueue(object : Callback<CurrentWeatherInfo> {
-                            override fun onResponse(
-                                call: Call<CurrentWeatherInfo>,
-                                response: Response<CurrentWeatherInfo>
-                            ) {
-                                currWeatherFragment.showCurrentWeather(response.body())
-                                Log.i(TAG, "onResponse: ${response.body().toString()}")
+                .addOnSuccessListener { it: Location? ->
+                    if (it != null) {
+                        location = it
+                        Log.i(TAG, "lastLocation: $location")
+                        showCurrentWeather()
+                    } else {
+                        // TODO: ask turn on gps (else can't determine location (?))
+                        //TODO: try/catch (if not determined -> error)
+                        Toast.makeText(this, "Location null, getting current location..", Toast.LENGTH_SHORT).show()
+                        fusedLocationClient.getCurrentLocation(PRIORITY_LOW_POWER, null).addOnSuccessListener {
+                            it?.let {
+                                Toast.makeText(this, "Location determined", Toast.LENGTH_SHORT).show()
+                                location = it
+                                Log.i(TAG, "getCurrentLocation: $location")
+                                showCurrentWeather()
                             }
-
-                            override fun onFailure(call: Call<CurrentWeatherInfo>, t: Throwable) {
-                                Log.e(TAG, "onFailure: ", t)
+                            if (it == null) {
+                                Toast.makeText(this, "Impossible get current location", Toast.LENGTH_SHORT).show()
+                                Log.i(TAG, "getCurrentLocation: $location")
                             }
-
-                        })
+                        }
+                    }
                 }
-
-
-            }
     }
 
-    override fun showForecast() {
-
-        forecastFragment = ForecastFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, forecastFragment)
-            .addToBackStack(null)
-            .commit()
+    private fun showCurrentWeather() {
 
         ServiceGenerator.getRequestApi()
-            .getForecast(
-                location.latitude,
-                location.longitude
-            )
-            .enqueue(object : Callback<ForecastJsonObj> {
-                override fun onResponse(
-                    call: Call<ForecastJsonObj>,
-                    response: Response<ForecastJsonObj>
-                ) {
-                    forecastFragment.showForecast(response.body()?.forecasts ?: listOf())
-                    Log.i(TAG, "onResponse: ${response.body().toString()}")
-                }
+                .getCurrentWeather(
+                        location.latitude,
+                        location.longitude
+                )
+                .enqueue(object : Callback<CurrentWeatherInfo> {
+                    override fun onResponse(
+                            call: Call<CurrentWeatherInfo>,
+                            response: Response<CurrentWeatherInfo>
+                    ) {
+                        cityName = response.body()?.cityName ?: getString(R.string.forecast)
+                        currentWeather = response.body()
+                        currWeatherFragment.showCurrentWeather(response.body())
+                        Log.i(TAG, "onResponse: ${response.body().toString()}")
+                    }
 
-                override fun onFailure(call: Call<ForecastJsonObj>, t: Throwable) {
-                    Log.e(TAG, "onFailure: ", t)
-                }
+                    override fun onFailure(call: Call<CurrentWeatherInfo>, t: Throwable) {
+                        Log.e(TAG, "onFailure: ", t)
+                    }
 
-            })
+                })
+    }
+
+    private fun showForecast() {
+
+        ServiceGenerator.getRequestApi()
+                .getForecast(
+                        location.latitude,
+                        location.longitude
+                )
+                .enqueue(object : Callback<ForecastJsonObj> {
+                    override fun onResponse(
+                            call: Call<ForecastJsonObj>,
+                            response: Response<ForecastJsonObj>
+                    ) {
+                        forecastFragment.showForecast(response.body()?.forecasts ?: listOf())
+                        Log.i(TAG, "onResponse: ${response.body().toString()}")
+                    }
+
+                    override fun onFailure(call: Call<ForecastJsonObj>, t: Throwable) {
+                        Log.e(TAG, "onFailure: ", t)
+                    }
+
+                })
     }
 
 }
